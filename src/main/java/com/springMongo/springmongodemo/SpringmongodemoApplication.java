e0b9a1a52c7b35885dc939cd288440eb5c8298f9
Vinodkumar Vijayabhaskar <vvinodkumar97@gmail.com>
2021-03-12 12:50:05 +0530

    Starting a spring mongo demo project

---
+++ src/main/java/com/springMongo/springmongodemo/SpringmongodemoApplication.java
@@ -0,0 +1,13 @@
+package com.springMongo.springmongodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongodemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springMongo/springmongodemo/SpringmongodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springMongo.springmongodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
